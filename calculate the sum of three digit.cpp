3ae3ee7cc2dbbef8805c059a249788f6899f8891
Ahmed Hesham Fayed <111004537+AhmedFayed123@users.noreply.github.com>
2023-01-02 15:18:02 +0200

    Add files via upload

---
+++ calculate the sum of three digit.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y,z;
+	cout<<"enter 1st number : ";
+	cin>>x;
+	cout<<"enter 2st number : ";
+	cin>>y;
+	cout<<"enter 3st number : ";
+	cin>>z;
+	cout<< "the sum =  "  << x+y+z;
+
+	return 0;
+	
+}
